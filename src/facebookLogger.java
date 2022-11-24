ca11fb0656de1fca348c480302679dd02bd263f2
Pedro Soares <34283375+pppedro173@users.noreply.github.com>
2022-11-24 09:39:06 +0000

    Update facebookLogger.java

--- src/facebookLogger.java
+++ src/facebookLogger.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class facebookLogger {
 
     public static void main(String[] args){
-        System.out.println("hello");
         WebDriver webDriver = new ChromeDriver();
 
         webDriver.get("https://www.facebook.com/");
